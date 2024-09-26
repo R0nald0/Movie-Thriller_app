df89f87091983cb1913dfc691e7fd3f6141a7b25
R0nald0 <ronaldo-172010@hotmail.com>
2024-09-25 22:43:39 -0300

    atualiado configurções

--- lib/data/Api/DioApi.dart
+++ lib/data/Api/DioApi.dart
@@ -1,18 +1,19 @@
 import 'package:dio/dio.dart';
-import 'package:get/get_connect/http/src/response/response.dart' hide Response;
-import 'package:get/get_rx/get_rx.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../../config/Config.dart';
-import '../../config/Key_config.dart';
 import 'IApi.dart';
 
 class DioApi implements IApi {
   final Dio client;
   DioApi(this.client);
 
+  @override
   Future<Response<dynamic>> getApi(String uriFilmes, [var pag = 1]) async {
+  final apiKey = dotenv.env['api_key'];
+
     var result = await client.get(
-        "${Config.ENDPOINT}$uriFilmes?api_key=${Key_config.API_KEY}&page=${pag}");
+        "${Config.ENDPOINT}$uriFilmes?api_key=$apiKey&page=$pag");
     return result;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:the_movie/MyApp.dart';
 
-void main() {
+void main() async {
+   await initialiaps();
   runApp(const MyApp());
+}
+
+Future<void> initialiaps() async {
+   await dotenv.load( fileName: '.env');
 }
